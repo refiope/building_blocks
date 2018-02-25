0c48f1f7d5bfdc3b9dc07b602d7f08d553654170
Patrick Kang <wintsunlight@gmail.com>
2018-02-25 15:06:04 +0900

    cleaned up unneccessary code in stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,5 +1,3 @@
-#> stock_picker([17,3,6,9,15,8,6,1,10])
-#=> [1,4]  # for a profit of $15 - $3 == $12
 def stock_picker (array)
   best_value = 0
   two_days = [0, 0]
@@ -11,11 +9,9 @@ def stock_picker (array)
         best_value = array[i + 1] - stock #if difference is higher than best_value, change best_value
         two_days[0] = index #store the two days
         two_days[1] = i + 1
-      else
-        best_value = best_value #keep the best_value if the original is higher
       end
       i += 1 #increment to check the rest of the values inside the array
     end }
 
-  puts two_days.inspect
+  print two_days.inspect
 end
